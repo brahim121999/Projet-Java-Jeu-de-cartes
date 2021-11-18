2d7897e6952955beb746a64a49ebe230d140a581
bob19 <e20b351x>
2021-11-18 11:11:02 +0100

    execute

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
 
 public class test {
 	int test;
 	
 	
 	String dernier_test;
+	
+	public static void main(String[] argv){
+		
+		System.out.print("execute");
+		
+		
+	}
 
 }
